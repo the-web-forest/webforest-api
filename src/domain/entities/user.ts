0f5164d1d174d0085447c60329c34034917e3779
Suelen <suelem_pira@hotmail.com>
2024-05-21 21:48:30 -0300

    creating entitites

---
+++ src/domain/entities/user.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('user')
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column({name: 'first_name'})
+    firstName: string;
+    @Column({name: 'last_name'})
+    lastName: string;
+    @Column({name: 'email'})
+    email: string;
+    @Column({name: 'password'})
+    password: string;
+    @Column({name: 'is_active', default: true})
+    isActive: boolean;
+    @Column({name: 'is_deleted', default: false})
+    isDeleted: boolean;
+    @Column({name: 'created_at'})
+    createdAt: Date;
+    @Column({name: 'updated_at'})
+    updatedAt: Date;
+}
